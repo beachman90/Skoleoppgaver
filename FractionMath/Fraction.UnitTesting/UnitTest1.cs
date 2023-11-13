d0def79646db1cf601c2170450ddb20a37ecfee8
beachman90 <jistrandman@gmail.com>
2023-11-13 08:46:43 +0100

    Created new project.

--- FractionMath/Fraction.UnitTesting/UnitTest1.cs
+++ FractionMath/Fraction.UnitTesting/UnitTest1.cs
@@ -20,9 +20,21 @@ namespace FractionMath.UnitTesting
             Fraction actual = fraction.FractionAdd(fraction2);
 
             Assert.That(actual, Is.EqualTo(expected));
-
-
         }
+
+        public void Multiply_TwoFraction_ReturnsExpectedSum()
+        {
+            Fraction fraction = new(2, 5);
+            Fraction fraction2 = new(2, 7);
+
+            Fraction expected = new(4, 35);
+
+            Fraction actual = fraction.FractionMultiply(fraction2);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+
 
         
     }
